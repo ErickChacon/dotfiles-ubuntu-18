d83bb79932223ee18eef8c66640d0abe55d96951
Erick Chacon Montalvan <eral.th07@gmail.com>
2018-09-03 14:54:20 +0100

    bashrc home

--- bashrc-docker.sh
+++ bashrc-docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Path to the bash it configuration
-export BASH_IT="/home/rstudio/.bash_it"
+export BASH_IT="~/.bash_it"
 
 # Lock and Load a custom theme file
 # location /.bash_it/themes/
