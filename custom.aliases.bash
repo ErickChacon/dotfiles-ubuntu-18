ea75441264f5bc09f524581e1f9a315cacc4e43e
Erick <eral.th07@gmail.com>
2018-09-09 00:26:18 +0100

    nvim, tmux, and my-docker

--- custom.aliases.bash
+++ custom.aliases.bash
@@ -2,7 +2,7 @@
 # cite 'about-alias'
 # about-alias 'Tmux terminal multiplexer'
 
-alias tx='tmux'
+# alias tx='docker exec -ti global-docker tmux'
 alias txl='tmux ls'
 alias txn='tmux new -s'
 alias txa='tmux a -t'
@@ -14,8 +14,32 @@ txo() {
   ./.tmux/proj-$1 $2
 }
 
+vm() {
+  current=$(pwd)
+  current=${current/"$HOME/"/}
+  docker exec -it -w /home/rstudio/$current global-docker nvim $1
+}
 
-alias vm="nvim"
+tx() {
+  current=$(pwd)
+  current=${current/"$HOME/"/}
+  docker exec -it -w /home/rstudio/$current global-docker tmux $1
+}
+
+my-docker() {
+  current=$(pwd)
+  current=${current/"$HOME/"/}
+  docker run --user rstudio --name ${1:global-docker} \
+  -v /home/chaconmo/Documents/:/home/rstudio/Documents \
+  -v /home/chaconmo/.bash_it/aliases/custom.aliases.bash:/home/rstudio/custom.alias.bash \
+  --rm -it -e "TERM=xterm-256color-italic" my-r bash
+}
+
+
+
+
+# alias vm="docker exec -ti global-docker nvim"
+alias R="docker exec -ti global-docker R"
 
 alias cdre='cd ~/Documents/Repositories'
 alias cdno='cd ~/Documents/NotesCodes'
