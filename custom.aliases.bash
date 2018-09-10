26efc8e4e3406b46e0d53668b39385badec7a081
Erick Chacon Montalvan <eral.th07@gmail.com>
2018-09-10 08:44:24 +0000

    updated aliases and dockerfile

--- custom.aliases.bash
+++ custom.aliases.bash
@@ -1,51 +1,62 @@
+cite 'about-alias'
+about-alias 'custom aliases and functions to work with docker'
 
-# cite 'about-alias'
-# about-alias 'Tmux terminal multiplexer'
+# pwd without home
+pwd_short() {
+  local current=$(pwd)
+  if [[ $current == $HOME ]]; then
+    current=""
+  else
+    current=${current/"$HOME"/}
+  fi
+  echo $current
+}
 
-# alias tx='docker exec -ti global-docker tmux'
-alias txl='tmux ls'
-alias txn='tmux new -s'
-alias txa='tmux a -t'
-alias txk='tmux kill-session -t'
-# alias txo='.tmux/proj-$1'
+# create docker container
+my-docker() {
+  local current=$(pwd_short)
+  XSOCK=/tmp/.X11-unix && \
+  XAUTH=/tmp/.docker.xauth && \
+  xauth nlist :0 | sed -e "s/^..../ffff/" | xauth -f $XAUTH nmerge - && \
+  docker run --user rstudio --name ${1:-global-docker} \
+  -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH \
+  -v /home/chaconmo/Documents/:/home/rstudio/Documents \
+  -v /home/chaconmo/Dropbox/:/home/rstudio/Dropbox \
+  -v /home/chaconmo/.ssh:/home/rstudio/.ssh \
+  -v /home/chaconmo/.gitconfig:/home/rstudio/.gitconfig \
+  -v /home/chaconmo/.bash_it:/home/rstudio/.bash-it \
+  -w /home/rstudio$current \
+  -e XAUTHORITY=$XAUTH  -e DISPLAY=$DISPLAY -e "TERM=xterm-256color-italic" \
+  --rm -it my-r bash
+}
+
+# execute bash on container
+bs() {
+  local current=$(pwd_short)
+  docker exec -it -w /home/rstudio$current global-docker bash
+}
+
+# execute nvim on container
+vm() {
+  local current=$(pwd_short)
+  docker exec -it -w /home/rstudio$current global-docker nvim $1
+}
+
+# execute tmux on container
+tx() {
+  local current=$(pwd_short)
+  docker exec -it -w /home/rstudio$current global-docker tmux
+}
+
+# execute R on container
+R() {
+  local current=$(pwd_short)
+  docker exec -it -w /home/rstudio$current global-docker R
+}
 
 txo() {
   # echo '.tmux/proj-'$1
   ./.tmux/proj-$1 $2
-}
-
-vm() {
-  current=$(pwd)
-  current=${current/"$HOME/"/}
-  docker exec -it -w /home/rstudio/$current global-docker nvim $1
-}
-
-tx() {
-  current=$(pwd)
-  current=${current/"$HOME/"/}
-  docker exec -it -w /home/rstudio/$current global-docker tmux $1
-}
-
-R() {
-  current=$(pwd)
-  current=${current/"$HOME/"/}
-  docker exec -it -w /home/rstudio/$current global-docker R
-}
-
-
-my-docker() {
-  current=$(pwd)
-  current=${current/"$HOME/"/}
-  XSOCK=/tmp/.X11-unix && \
-  XAUTH=/tmp/.docker.xauth && \
-  xauth nlist :0 | sed -e "s/^..../ffff/" | xauth -f $XAUTH nmerge - && \
-  docker run --user rstudio --name ${1:global-docker} \
-  -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH \
-  -v /home/chaconmo/Documents/:/home/rstudio/Documents \
-  -v /home/chaconmo/.ssh:/home/rstudio/.ssh \
-  -v /home/chaconmo/.gitconfig:/home/rstudio/.gitconfig \
-  -e XAUTHORITY=$XAUTH  -e DISPLAY=$DISPLAY -e "TERM=xterm-256color-italic" \
-  --rm -it my-r bash
 }
 
   # docker run --user rstudio --name ${1:global-docker} \
@@ -56,6 +67,14 @@ my-docker() {
 
 # alias vm="docker exec -ti global-docker nvim"
 # alias R="docker exec -ti global-docker R"
+
+# alias tx='docker exec -ti global-docker tmux'
+alias txl='tmux ls'
+alias txn='tmux new -s'
+alias txa='tmux a -t'
+alias txk='tmux kill-session -t'
+# alias txo='.tmux/proj-$1'
+
 
 alias cdre='cd ~/Documents/Repositories'
 alias cdno='cd ~/Documents/NotesCodes'
