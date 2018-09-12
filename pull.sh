9b1eb011ab7da62877e9edf82adb25a431e21414
Erick Chacon Montalvan <eral.th07@gmail.com>
2018-09-12 13:50:17 +0100

    pull and push dotfiles

--- pull.sh
+++ pull.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 if [[ $HOSTNAME == "chaconmo-ThinkPad-L470-W10DG" ]]; then
   echo "Working at Lancaster University!"
 elif [[ $HOSTNAME == "chaconmo-Precision-5510" ]]; then
@@ -7,25 +8,9 @@ fi
 
 # Terminal
 cp .bashrc ~/.bashrc
+cp .bash_profile ~/.bash_profile
 cp custom.aliases.bash ~/.bash_it/aliases/custom.aliases.bash
 
-# Neovim
-cp -r nvim ~/.config/
-cp -r UltiSnips ~/Documents/Nvim/
-
-# R
-sudo cp  Rprofile.site /etc/R/Rprofile.site
-cp .Rprofile ~/.Rprofile
-cp .ctags ~/.ctags
-
 # Unison
-mkdir ~/.unison
+mkdir -p ~/.unison
 cp default.prf ~/.unison/default.prf
-
-# Tmux
-cp .tmux.conf ~/.tmux.conf
-cp -r .tmux ~/
-
-# Oni
-mkdir ~/.oni
-cp oni-config.js ~/.oni/config.js
--- push.sh
+++ push.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 if [[ $HOSTNAME == "chaconmo-ThinkPad-L470-W10DG" ]]; then
   echo "Working at Lancaster University!"
 elif [[ $HOSTNAME == "chaconmo-Precision-5510" ]]; then
@@ -7,23 +8,8 @@ fi
 
 # Terminal
 cp ~/.bashrc .bashrc
+cp ~/.bash_profile .bash_profile
 cp ~/.bash_it/aliases/custom.aliases.bash custom.aliases.bash
-
-# Neovim
-cp -rT ~/.config/nvim nvim
-cp -rT ~/Documents/Nvim/UltiSnips UltiSnips
-
-# R
-cp /etc/R/Rprofile.site Rprofile.site
-cp ~/.Rprofile .Rprofile
-cp ~/.ctags .ctags
 
 # Unison
 cp ~/.unison/default.prf default.prf
-
-# Tmux
-cp ~/.tmux.conf .tmux.conf
-cp -rT ~/.tmux .tmux
-
-# Oni
-cp ~/.oni/config.js oni-config.js
