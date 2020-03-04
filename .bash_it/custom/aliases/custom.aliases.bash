ba2ae4245650cd436c27a5fddbe6b15eb64317e1
Erick Chacon Montalvan <eral.th07@gmail.com>
2020-03-04 16:31:48 +0000

    bash_it custom docker

--- .bash_it/custom/aliases/custom.aliases.bash
+++ .bash_it/custom/aliases/custom.aliases.bash
@@ -38,8 +38,8 @@ start-docker() {
   -v $HOME/.palettes:/home/rstudio/.palettes \
   -v $HOME/.config/mutt:/home/rstudio/.config/mutt \
   -v /tmp/local:/tmp/local \
-  -v $dot_dir/custom.plugins.bash:/home/rstudio/.bash_it/plugins/custom.plugins.bash \
-  -v $dot_docker/custom.aliases.bash:/home/rstudio/.bash_it/aliases/custom.aliases.bash \
+  -v $dot_dir/.bash_it/custom:/home/rstudio/.bash_it/custom
+    # custom.plugins.bash:/home/rstudio/.bash_it/plugins/custom.plugins.bash \
   -v $dot_docker/.bashrc:/home/rstudio/.bashrc \
   -v $dot_docker/.bash_profile:/home/rstudio/.bash_profile \
   -v $dot_dir_g/.scripts:/home/rstudio/.scripts \
@@ -53,6 +53,9 @@ start-docker() {
   -e XAUTHORITY=$XAUTH  -e DISPLAY=$DISPLAY -e "TERM=xterm-256color-italic" \
   --rm -it ${3:-erickchacon/stat-toolbox:3.6.2} bash
 }
+
+  # -v $dot_dir/custom.plugins.bash:/home/rstudio/.bash_it/plugins/custom.plugins.bash \
+  # -v $dot_docker/custom.aliases.bash:/home/rstudio/.bash_it/aliases/custom.aliases.bash \
 
 start-docker-rstudio() {
   mkdir -p /tmp/local
