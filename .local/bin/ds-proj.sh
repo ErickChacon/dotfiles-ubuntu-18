724f1ea32bca8e87715928113c3eefde30cdd464
Erick Chacon Montalvan <eral.th07@gmail.com>
2020-03-13 22:55:23 +0000

    ds-proj

--- .local/bin/ds-proj.sh
+++ .local/bin/ds-proj.sh
@@ -9,3 +9,11 @@ touch 40-src/{10-extract,20-clean,30-process,40-explore,50-model,60-summarise}.R
 mkdir -p 50-scripts/{10-extract,20-clean,30-process,40-explore,50-model,60-summarise}
 mkdir -p 60-outputs/{10-extract,20-clean,30-process,40-explore,50-model,60-summarise}
 
+
+touch {10-planning/.keep,20-literature/.keep,,70-publication/.keep}
+touch 30-data/{10-raw/.keep,20-cleaned/.keep,30-processed/.keep}
+touch \
+    50-scripts/{10-extract/.keep,20-clean/.keep,30-process/.keep,40-explore/.keep,50-model/.keep,60-summarise/.keep}
+touch \
+    60-outputs/{10-extract/.keep,20-clean/.keep,30-process/.keep,40-explore/.keep,50-model/.keep,60-summarise/.keep}
+
