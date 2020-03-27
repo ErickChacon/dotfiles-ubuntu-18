1c37ca2319f9aae3a1c516cb73a07a7635688089
Erick Chacon <eral.th07@gmail.com>
2020-03-27 19:34:33 +0000

    dwm

--- ubuntu-dwm.sh
+++ ubuntu-dwm.sh
@@ -1,5 +1,4 @@
-sudo apt-get install -y xorg libx11-dev libxft-dev libxinerama-dev suckless-tools dmenu
-# xdm
+sudo apt-get install -y libx11-dev libxft-dev libxinerama-dev suckless-tools dmenu
 git clone https://git.suckless.org/dwm
 # gdm3(defualt) xdm : /etc/init.d/
 cd dwm
