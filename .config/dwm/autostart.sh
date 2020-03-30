91da5312154f3b4fcbe008375a6aeb368aeee63b
Erick Chacon <eral.th07@gmail.com>
2020-03-30 23:50:48 +0100

    monitors for dwm

--- .config/dwm/autostart.sh
+++ .config/dwm/autostart.sh
@@ -1,15 +1,33 @@
 #!/bin/sh
 
-# /usr/lib/gnome-settings-daemon/gsd-xsettings # gnome settings
-# gnome-flashback
-
 monitors=$(xrandr | awk '/ connected/ { print $1}')
 monitors_len=$(echo $monitors | wc -w)
+only_one=${1:-true}
+internal=${2:-false}
 
 if [ $monitors_len -eq 2 ]; then
-    xrandr \
-        --output DP-1 --mode 3440x1440 --scale 1.5x1.5 --pos 0x0 --primary \
-        --output eDP-1 --off
+    if [ $only_one = "true" ]; then
+        if [ $internal = "true" ]; then
+            xrandr \
+                --output DP-1 --off \
+                --output eDP-1 --mode 3840x2160 --scale 1x1 --pos 0x0 --primary 
+        else
+            xrandr \
+                --output DP-1 --mode 3440x1440 --scale 1.5x1.5 --pos 0x0 --primary \
+                --output eDP-1 --off
+        fi
+    else
+        if [ $internal = "true" ]; then
+            xrandr \
+                --output eDP-1 --mode 3840x2160 --scale 1x1 --pos 0x0 --primary \
+                --output DP-1 --mode 3440x1440 --scale 1.5x1.5 --pos 3840x0
+        else
+            xrandr \
+                --output DP-1 --mode 3440x1440 --scale 1.5x1.5 --pos 0x0 --primary \
+                --output eDP-1 --mode 3840x2160 --scale 1x1 --pos 5160x0
+        fi
+    fi
 fi
 
 feh --bg-fill "$HOME/.config/wall.png"
+# /usr/bin/setxkbmap -option "ctrl:swapcaps"
