7c2b184ffae08519daff7f0c4f7eeafe084572a4
Erick Chacon <eral.th07@gmail.com>
2020-09-21 03:27:00 +0100

    fix bw project and vpn

--- .bash_it/custom/plugins/custom.plugins.bash
+++ .bash_it/custom/plugins/custom.plugins.bash
@@ -159,6 +159,7 @@ printf "%s\n" "${colors_name[@]}" | paste - $colors | \
 
 vpn() {
   # nm-applet
-  nmcli --ask con up id "Lancaster University VPN"
+  # nmcli --ask con up id "Lancaster University VPN"
+  nmcli --ask con up id "vpn"
 }
 
